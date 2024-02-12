45afc56ceff2be2fca8398fb61f3ae1a9542e9e7
toyman640 <falakoadebayor@gmail.com>
2024-02-12 12:36:38 +0100

    Set up component

---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => (
+  <div>
+    <h2>This is the login components</h2>
+  </div>
+)
+
+export default Login;
---
+++ src/components/SignUp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignUp = () => (
+  <div>
+    <h2>This is the sign up components</h2>
+  </div>
+)
+
+export default SignUp;
